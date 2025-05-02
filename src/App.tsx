64bb64e2c21368a48d2d8ee829ffb6baf2e80b10
Brayan Reyes <rmbd0603@email.com>
2025-05-02 16:26:50 -0500

    created profile

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import "./App.css";
+import PersonalInformation from "./PersonalInformation/PersonalInformation";
+import Education from "./Education/Education";
+import Achievements from "./Achievements/Achievements";
+import Icons from "./Icons/Icons";
+import Experience from "./Experience/Experience";
+import Interests from "./Interests/Interests";
+
+const Profile = () => {
+  const contentColor = "#f0e3d3";
+  const titleColor = "#fbf0e5";
+
+  const titles = [
+    {
+      title: "Personal information",
+      link: "#personalInformation",
+    },
+    {
+      title: "Experience",
+      link: "#experience",
+    },
+    {
+      title: "Education",
+      link: "#education",
+    },
+    {
+      title: "Achievements",
+      link: "#achievements",
+    },
+    {
+      title: "Interests",
+      link: "#interests",
+    },
+  ];
+
+  return (
+    <main className="mainContainer">
+      <section className="menuProfile">
+        <img
+          src="/profile.png"
+          alt="Signature uploaded"
+          className="imageProfile"
+        />
+        <section className="profileSection">
+          {titles.map(({ title, link }) => (
+            <nav>
+              <a
+                href={link}
+                key={title}
+                style={{
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  color: "#ffffff",
+                }}
+              >
+                {title}
+              </a>
+            </nav>
+          ))}
+        </section>
+      </section>
+      <section
+        style={{
+          overflowX: "scroll",
+        }}
+      >
+        <PersonalInformation
+          titleColor={titleColor}
+          contentColor={contentColor}
+        />
+        <Experience titleColor={titleColor} contentColor={contentColor} />
+        <Education titleColor={titleColor} contentColor={contentColor} />
+        <Achievements titleColor={titleColor} contentColor={contentColor} />
+        <Interests titleColor={titleColor} contentColor={contentColor} />
+        <Icons />
+      </section>
+    </main>
+  );
+};
+
+export default Profile;
