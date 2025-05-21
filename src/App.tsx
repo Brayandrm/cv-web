56d589b725ff712765c389ff5dfc9b8dfd7d3914
Brayan Reyes <rmbd0603@email.com>
2025-05-21 15:42:48 -0500

    feat: added loading

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import Achievements from "./Achievements/Achievements";
 import Icons from "./Icons/Icons";
 import Experience from "./Experience/Experience";
 import Interests from "./Interests/Interests";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaCircleArrowUp } from "react-icons/fa6";
 import { IoIosMenu } from "react-icons/io";
+import "../src/Common/Loading/Loading.css";
 
 const Profile = () => {
   const [selected, setSelected] = useState("Personal information");
 
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const [loading, setLoading] = useState(true);
 
   const titles = [
     {
@@ -37,56 +40,71 @@ const Profile = () => {
     },
   ];
 
-  return (
-    <main className="mainContainer">
-      <section
-        className={`menuProfile ${menuOpen ? "menuOpen" : "menuClosed"}`}
-      >
-        <img
-          src="/Brayan.png"
-          alt="Signature uploaded"
-          className="imageProfile"
-        />
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500);
 
-        <section className="profileSection">
-          {titles.map(({ title, link }) => (
-            <nav key={title}>
-              <a
-                href={link}
-                onClick={() => {
-                  setSelected(title);
-                  setMenuOpen(false);
-                }}
-                className={`menuProfileLink ${
-                  selected === title ? "activeLink" : "inactiveLink"
-                }`}
-              >
-                {title}
-              </a>
-            </nav>
-          ))}
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main>
+      {loading ? (
+        <article className="loaderContainer">
+          <span className="loader">Load&nbsp;ng</span>
+        </article>
+      ) : (
+        <section className="mainContainer">
+          <section
+            className={`menuProfile ${menuOpen ? "menuOpen" : "menuClosed"}`}
+          >
+            <img
+              src="/Brayan.png"
+              alt="Signature uploaded"
+              className="imageProfile"
+            />
+            <section className="profileSection">
+              {titles.map(({ title, link }) => (
+                <nav key={title}>
+                  <a
+                    href={link}
+                    onClick={() => {
+                      setSelected(title);
+                      setMenuOpen(false);
+                    }}
+                    className={`menuProfileLink ${
+                      selected === title ? "activeLink" : "inactiveLink"
+                    }`}
+                  >
+                    {title}
+                  </a>
+                </nav>
+              ))}
+            </section>
+          </section>
+          <button aria-label="button arrow" className="btn">
+            <FaCircleArrowUp className="btn buttonMobile" />
+          </button>
+          <section className="headerMobile">
+            <button
+              aria-label="button menu"
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="headerButtonMenu "
+            >
+              <IoIosMenu className="iconMenu" />
+            </button>
+          </section>
+          <section className={`scrollableSection ${menuOpen ? "blurred" : ""}`}>
+            <PersonalInformation />
+            <Experience />
+            <Education />
+            <Achievements />
+            <Interests />
+            <Icons />
+          </section>
         </section>
-      </section>
-      <button aria-label="button arrow" className="btn">
-        <FaCircleArrowUp className="btn buttonMobile" />
-      </button>
-      <section className="headerMobile">
-        <button
-          aria-label="button menu"
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="headerButtonMenu "
-        >
-          <IoIosMenu className="iconMenu" />
-        </button>
-      </section>
-      <section className={`scrollableSection ${menuOpen ? "blurred" : ""}`}>
-        <PersonalInformation />
-        <Experience />
-        <Education />
-        <Achievements />
-        <Interests />
-        <Icons />
-      </section>
+      )}
     </main>
   );
 };
