d1b99c100cced2b6e08831b56eef705ec290b77b
Brayan Reyes <rmbd0603@email.com>
2025-06-09 15:14:10 -0500

    fix: stash

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,13 +8,16 @@ import CvPage from "./pages/Cvpage/CvPage";
 import Profile from "./pages/ProfilePage/ProfilePage";
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/profilePage" replace />} />
-      <Route path="/profilePage" element={<Profile />} />
-      <Route path="/cvWeb" element={<CvPage />} />
-    </Routes>
-  </Router>
+  <div>
+    <Profile />
+  </div>
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<Navigate to="/profilePage" replace />} />
+  //     <Route path="/profilePage" element={<Profile />} />
+  //     <Route path="/cvWeb" element={<CvPage />} />
+  //   </Routes>
+  // </Router>
 );
 
 export default App;
